3b05d53d93eabad6a5ef3d70a99eae8de40a3f4c
SairajGovilkar <rajgov408@gmail.com>
2024-01-31 01:55:43 +0530

    upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "a8afc6aba557da47975730cd0a816b9f",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
